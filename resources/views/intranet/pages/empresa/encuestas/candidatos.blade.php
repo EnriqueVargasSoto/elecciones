c9584118c23a16fef11a40c51727173cff0f5106
EnriqueVargasSoto <kikevargas23@gmail.com>
2022-07-26 17:15:19 -0500

    26/07/2022 17.15

--- resources/views/intranet/pages/empresa/encuestas/candidatos.blade.php
+++ resources/views/intranet/pages/empresa/encuestas/candidatos.blade.php
@@ -41,7 +41,9 @@
                 @foreach ($candidatos as $key => $candidato)
                     <tr>
                       <td class="text-sm font-weight-normal">
-                        {{$candidato}}
+                        <a href="{{ route('candidatos.delete', $candidato->id)}}" class="btn btn-danger"> Eliminar </a>
+                            <button type="button" class="btn btn-success" style="float: right" data-bs-toggle="modal" data-bs-target="#exampleModalEdit{{$key}}">Editar</button>
+                        
                       </td>
                       
                     
